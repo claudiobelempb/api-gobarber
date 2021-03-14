3b785bf729d6ae52cc5ccb3f32ba71b80a370bf4
claudiobelempb <claudio.c.lima@hotmail.com>
2021-03-14 16:40:54 -0300

    router appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,65 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface IAppointments {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: IAppointments[] = [];
 
 appointmentsRouter.get('/', (request, response) => {
-  return response.json({method: `GET:`});
+
+  return response.json(appointments);
 });
 
 appointmentsRouter.get('/:id', (request, response) => {
-  return response.json({method: `GET:ID - ${request.params.id}`});
+  const { id } = request.params;
+
+  const appointment = appointments.find(appointment => appointment.id === id);
+
+  if(!appointment) {
+    return response.status(404).json({error: 'Appointment not found'});
+  }
+
+  return response.json(appointment);
 })
 
 appointmentsRouter.post('/', (request, response) => {
 
   const { provider, date } = request.body;
 
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parseDate, appointment.date));
+
+  if(findAppointmentInSameDate){
+    return response.status(404).json({error: 'This appointment is already book'})
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date
+    date: parseDate
   }
+
+  appointments.push( appointment );
 
   return response.json(appointment);
 
 });
 
 appointmentsRouter.put('/:id', (request, response) => {
+  const { id } = request.params;
+  const { provider, date } = request.body;
+
+  const appointmentIndex = appointments.findIndex(appointment => appointment.id === id);
+
+
   return response.json({method: `PUT:ID - ${request.params.id}`});
 });
 
